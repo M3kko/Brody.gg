28495003673201afaba12c7ca6288520a0a61499
m3kko <Brody@imaximus.com>
2025-06-01 20:11:34 -0500

    testing new links

--- api/charm-roulette.js
+++ api/charm-roulette.js
@@ -2,9 +2,10 @@
 
 export default function handler(req, res) {
     const links = [
-      'https://discord.com/channels/@me/1278908744335626370/1378900109236113528',
-      'https://discord.com/channels/@me/1278908744335626370/1299984795274711130',
-      'https://discord.com/channels/@me/1278908744335626370/1375160685473959967'
+      'discord://channels/@me/1278908744335626370/1378900109236113528',
+      'discord://channels/@me/1278908744335626370/1299984795274711130',
+      'discord://channels/@me/1278908744335626370/1375160685473959967'
+
     ];
   
     const pick = links[Math.floor(Math.random() * links.length)];
