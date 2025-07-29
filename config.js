c8ee7c869fb0f94a4731f601c447cefa1078ab2f
M3kko <Brody@imaximus.com>
2025-07-28 20:12:57 -0500

    updated config

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_AUTH_DOMAIN_HERE",
-    projectId: "YOUR_PROJECT_ID_HERE",
-    storageBucket: "YOUR_STORAGE_BUCKET_HERE",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-    appId: "YOUR_APP_ID_HERE"
+    apiKey: "YOUR_NEW_API_KEY_FROM_GOOGLE_CLOUD", // ← Paste your new key here
+    authDomain: "moon-messages.firebaseapp.com",
+    projectId: "moon-messages",
+    storageBucket: "moon-messages.firebasestorage.app",
+    messagingSenderId: "541321527834",
+    appId: "1:541321527834:web:1e3881f0675c9d74d787a3",
+    measurementId: "G-G7GBN03N30"
 };
